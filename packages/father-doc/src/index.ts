bfb1041c042defcc125026b7e3230601033c021a
PeachScript <scdzwyxst@gmail.com>
2019-11-05 19:44:16 +0800

    feat(father-doc): add entry to bootstrap umi dev with self plugin

--- packages/father-doc/src/index.ts
+++ packages/father-doc/src/index.ts
@@ -1,3 +1,36 @@
-export default () => {
-  console.log('hello father-doc');
+import { fork } from 'child_process';
+
+// for dev
+start();
+
+export default function start() {
+  process.env.PAGES_PATH = 'src';
+  process.env.UMI_PLUGINS = require.resolve('umi-plugin-father-doc');
+
+  // start umi use child process
+  const child = fork(
+    require.resolve('umi/bin/umi'),
+    ['dev', ...process.argv.slice(3)],
+    {
+      stdio: 'inherit'
+    },
+  );
+
+  // handle exit signals
+  child.on('exit', (code, signal) => {
+    if (signal === 'SIGABRT') {
+      process.exit(1);
+    }
+
+    process.exit(code);
+  });
+
+  process.on('SIGINT', () => {
+    child.kill('SIGINT');
+  });
+
+  process.on('SIGTERM', () => {
+    child.kill('SIGTERM');
+  });
 }
+
