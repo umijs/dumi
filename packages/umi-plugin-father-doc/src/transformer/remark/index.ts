36b04961b22461fa3ee2165ffa13fb051fe8316c
PeachScript <scdzwyxst@gmail.com>
2019-11-13 17:33:16 +0800

    feat(umi-plugin): add slug & heading link support

--- packages/umi-plugin-father-doc/src/transformer/remark/index.ts
+++ packages/umi-plugin-father-doc/src/transformer/remark/index.ts
@@ -1,6 +1,8 @@
 import unified from 'unified';
-import stringify from 'rehype-stringify';
 import frontmatter from 'remark-frontmatter';
+import stringify from 'rehype-stringify';
+import slug from 'rehype-slug';
+import headings from 'rehype-autolink-headings';
 import prism from '@mapbox/rehype-prism';
 import parse from './parse';
 import rehype from './rehype';
@@ -15,6 +17,16 @@ export default (raw: string, dir: string) => {
     .use(yaml)
     .use(rehype, { dir })
     .use(stringify, { allowDangerousHTML: true })
+    .use(slug)
+    .use(headings, {
+      content: {
+        type: 'element',
+        tagName: 'span',
+        properties: { className: ['octicon', 'octicon-link'] },
+        children: [{ type: 'text', value: '#' }]
+      },
+      properties: { className: 'anchor' },
+    })
     .use(prism)
     .use(jsx)
     .use(isolation);
