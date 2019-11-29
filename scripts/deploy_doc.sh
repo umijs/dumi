2adcd616d0194f96b953dd2bd68180e300e4f60e
PeachScript <scdzwyxst@gmail.com>
2019-11-29 18:35:05 +0800

    chore: add doc deploy script

---
+++ scripts/deploy_doc.sh
@@ -0,0 +1,24 @@
+
+set -e
+shopt -s extglob
+
+TEMP_PATH="docs/.temp"
+
+# build docs
+npm run doc:build
+
+# prepare deploy
+mkdir $TEMP_PATH
+cd $TEMP_PATH
+git init
+git pull git@github.com:umijs/father-doc.git gh-pages
+cp -r ../../dist/* .
+
+# commit and push changes
+git add -A
+git commit --am -m "build: deploy documentation"
+git push -f git@github.com:umijs/father-doc.git master:gh-pages
+
+# clean
+cd -
+rm -rf $TEMP_PATH
