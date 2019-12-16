915dd74237d5a4c8d54ec8b369774d8f62553b30
xiaohuoni <448627663@qq.com>
2019-12-16 17:17:13 +0800

    eslint fix

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   testPathIgnorePatterns: ['/packages/father-doc/lib/', '/packages/umi-plugin-father-doc/lib/'],
-  collectCoverageFrom: [`packages/**/src/**/*.{js,jsx,ts,tsx}`, '!**/fixtures/**'],
+  collectCoverageFrom: ['packages/**/src/**/*.{js,jsx,ts,tsx}', '!**/fixtures/**'],
 };
