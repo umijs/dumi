16758bac685bc3179a0f322ec9d80fdf236642d6
PeachScript <scdzwyxst@gmail.com>
2019-12-23 23:55:24 +0800

    perf(umi-plugin): optimize logic for decorateRoutes

--- packages/umi-plugin-father-doc/src/routes/decorateRoutes.ts
+++ packages/umi-plugin-father-doc/src/routes/decorateRoutes.ts
@@ -4,127 +4,119 @@ import deepmerge from 'deepmerge';
 import slash from 'slash2';
 import getFrontMatter from './getFrontMatter';
 
-function routeSorter(prev, next) {
-  const prevOrder = typeof prev.meta.order === 'number' ? prev.meta.order : 0;
-  const nextOrder = typeof next.meta.order === 'number' ? next.meta.order : 0;
+function isNestedRoute(route: IRoute) {
+  const parsed = typeof route.component === 'string' ? path.parse(route.component) : null;
 
-  return prevOrder === nextOrder ? 0 : nextOrder - prevOrder;
+  return (
+    // at least 2-level path
+    route.path.lastIndexOf('/') !== 0 ||
+    // or component filename is the default entry
+    (
+      parsed &&
+      route.path.length > 1 &&
+      /^(index|readme)$/.test(parsed.name)
+    )
+  );
 }
 
 /**
- * make standard umi routes has meta & group
- * @note  route will be decorated following contents:
- *        - add meta data that generated from markdown file
+ * make standard umi routes can be used by father-doc
+ * @note  route will be decorated the following contents:
+ *        - add meta from fallback meta & frontmatter
  *        - add title field & TitleWrapper to display page title
- *        - sort routes & group by order field
- *        - group routes by group field & directory nest
+ *        - correct route path by group path (replace prefix)
+ *        - flat child routes to the top-level routes
+ *        - add index routes for group which has no index route
  */
 export default function decorateRoutes(
   routes: IRoute[],
   paths: IApi['paths'],
   parentRoute?: IRoute,
 ) {
-  // read yaml config for current level routes
-  routes.forEach(route => {
-    route.meta = route.component
-      ? deepmerge(route.meta, getFrontMatter(route.component as string))
-      : {};
+  const redirects: IRoute[] = [];
+  const result = routes.reduce((total, route) => {
+    const frontMatter = typeof route.component === 'string' ? getFrontMatter(route.component) : {};
+    const fallbackMeta: any = {};
 
-    // apply yaml title
+    // generate fallback group meta for nest route
+    if (isNestedRoute(route)) {
+      const groupPath = route.path.match(/^([^]+?)(\/[^/]+)?$/)[1];
+
+      fallbackMeta.group = {
+        path: groupPath,
+      };
+    }
+
+    // set fallback title for route
+    if (typeof route.component === 'string') {
+      fallbackMeta.title = path.parse(route.component).name.replace(/^[a-z]/, s => s.toUpperCase());
+    }
+
+    // merge meta for route
+    route.meta = deepmerge(
+      fallbackMeta,
+      frontMatter,
+      (route.meta || {}), // allow user override meta via configuration routes
+    );
+
+    // fallback group title if there only has group path
+    if(route.meta.group?.path && !route.meta.group.title) {
+      route.meta.group.title = route.meta.group.path
+        // discard start slash
+        .replace(/^\//g, '')
+        // upper case the first english letter
+        .replace(/^[a-z]/, s => s.toUpperCase());
+    }
+
+    // apply meta title
     route.title = route.meta.title;
 
     // apply TitleWrapper
     // see also: https://github.com/umijs/umi/blob/master/packages/umi-plugin-react/src/plugins/title/index.js#L37
-    route.Routes = (route.Routes || []).concat(
-      slash(path.relative(paths.cwd, path.join(paths.absTmpDirPath, './TitleWrapper.jsx'))),
+    route.Routes = route.Routes || [];
+    route.Routes.push(
+      `./${slash(path.relative(paths.cwd, path.join(paths.absTmpDirPath, './TitleWrapper.jsx')))}`,
     );
+
+    // unshift to Routes if parent route has component
+    if (typeof parentRoute?.component === 'string') {
+      route.Routes.unshift(parentRoute.component);
+    }
+
+    // correct route path by group path
+    if (route.meta.group?.path && route.path !== route.meta.group.path) {
+      route.path = slash(path.join(route.meta.group.path, route.path.match(/([^/]*)$/)[1]));
+    }
+
+    // flat child routes
+    if (route.routes) {
+      total.push(...decorateRoutes(route.routes, paths, route));
+    } else {
+      total.push(route);
+    }
+
+    return total;
+  }, [] as IRoute[]);
+
+  // add index route redirect for group which has no index route
+  result.forEach((route) => {
+    if (
+      route.meta.group?.path &&
+      !result.some(item => item.path === route.meta.group.path)
+    ) {
+      const { title, path, ...resGroupMeta } = route.meta.group;
+
+      redirects.push({
+        title,
+        path,
+        meta: {
+          ...resGroupMeta
+        },
+        exact: true,
+        redirect: result.find(item => item.meta.group?.path === route.meta.group.path).path,
+      });
+    }
   });
 
-  // split grouped & ungrouped routes for current level routes
-  const [ungrouped, groupedMapping] = routes.reduce(
-    (result, item) => {
-      if (item.meta.group?.path) {
-        item.meta.group.path = slash(item.meta.group.path);
-        // prefix parent route for route
-        item.path = slash(path.join(item.meta.group.path, item.path));
-
-        if (!parentRoute) {
-          // only process top level group routes
-          const key = item.meta.group.path;
-
-          if (result[1][key]) {
-            result[1][key].push(item);
-          } else {
-            result[1][key] = [item];
-          }
-        } else {
-          // correct parent route path if it is not top level group
-          // can be use to correct path for automatic parent route from directory
-          parentRoute.path = item.meta.group.path;
-          result[0].push(item);
-        }
-      } else {
-        // prefix parent route for route
-        if (parentRoute) {
-          item.path = path.join(parentRoute.path, item.path).replace(/(\/|\\)$/, '');
-        }
-
-        item.path = slash(item.path);
-
-        result[0].push(item);
-      }
-
-      return result;
-    },
-    [[], {}],
-  );
-
-  // process child routes for ungropued routes if there is top level
-  if (!parentRoute) {
-    ungrouped.forEach(route => {
-      if (route.routes) {
-        route.routes = decorateRoutes(route.routes, paths, route);
-        // redirect to the first child route when visit root path
-        route.routes.push({
-          path: route.path,
-          redirect: route.routes[0].path,
-        });
-
-        // use group title for parent routes
-        if (route.routes[0].meta?.group?.title) {
-          route.meta.title = route.routes[0].meta.group.title;
-        }
-      }
-    });
-  }
-
-  // concat ungrouped routes & grouped items
-  const result: IRoute[] = ungrouped.concat(
-    Object.keys(groupedMapping).map(groupedPath => {
-      // find first configured child
-      const configuredChild = groupedMapping[groupedPath].find(item => item.meta.group);
-      const groupTitle = configuredChild
-        ? configuredChild.meta.group.title
-        : groupedPath.replace(/^\/|\/$/g, '');
-      const groupOrder = configuredChild ? configuredChild.meta.group.order : undefined;
-
-      return {
-        path: groupedPath,
-        routes: groupedMapping[groupedPath].concat({
-          path: groupedPath,
-          // redirect to the first child route when visit root path
-          redirect: groupedMapping[groupedPath][0].path,
-        }),
-        meta: {
-          title: groupTitle,
-          order: groupOrder,
-        },
-      };
-    }),
-  );
-
-  // sort routes by order field
-  result.sort(routeSorter);
-
-  return result;
+  return result.concat(redirects);
 }
