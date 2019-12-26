229ceec38d6e06c13370c03d210c26334c5549c0
PeachScript <scdzwyxst@gmail.com>
2019-12-26 14:07:40 +0800

    fix(umi-plugin): slug text bug if using code in title

--- packages/umi-plugin-father-doc/src/transformer/remark/header.ts
+++ packages/umi-plugin-father-doc/src/transformer/remark/header.ts
@@ -1,30 +1,23 @@
 import visit from 'unist-util-visit';
+import toString from 'hast-util-to-string';
 import has from 'hast-util-has-property';
 import is from 'hast-util-is-element';
 
 const headings = ['h2', 'h3']; // 先只取两级
 
-const extractText = header =>
-  header.children
-    .map(x => {
-      if (x.type === 'text') {
-        return x.value;
-      }
-      return '';
-    })
-    .join('');
-
 export default () => (ast, vFile) => {
-  if (vFile.data.slugs !== false) {
+  const slugsEnable = vFile.data.slugs !== false;
+
+  // initial slugs meta
+  vFile.data.slugs = [];
+
+  // extract if user did not set slugs to false
+  if (slugsEnable) {
     visit(ast, 'element', node => {
       if (is(node, headings) && has(node, 'id')) {
-        if (!vFile.data.slugs) {
-          vFile.data.slugs = [];
-        }
-
         vFile.data.slugs.push({
           depth: parseInt(node.tagName[1], 10) - 1,
-          value: extractText(node),
+          value: toString(node),
           heading: (node.properties as any)?.id,
         });
       }
