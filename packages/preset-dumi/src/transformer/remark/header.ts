b44f0b0576e5e3ea87638852f187d248f6202175
PeachScript <scdzwyxst@gmail.com>
2020-04-07 12:05:44 +0800

    fix(preset): slug collect error when using component in title

--- packages/preset-dumi/src/transformer/remark/header.ts
+++ packages/preset-dumi/src/transformer/remark/header.ts
@@ -5,13 +5,36 @@ import is from 'hast-util-is-element';
 
 const headings = ['h1', 'h2', 'h3', 'h4', 'h5'];
 
+function excludeComponentFromChildren(children) {
+  const rawStack = [];
+
+  return children.filter(item => {
+    if (item.type === 'raw') {
+      // ignore self-closing raw node, like <img />
+      if (/^<[A-Z][^\/]+>$/.test(item.value)) {
+        rawStack.push(item.value);
+      } else if (/^<\/[A-Z]/.test(item.value)) {
+        rawStack.pop();
+      }
+
+      return false;
+    }
+
+    // discard children if it was wrapped by built-in Component
+    return !rawStack.length;
+  });
+}
+
 export default () => (ast, vFile) => {
   // initial slugs meta
   vFile.data.slugs = [];
 
   visit(ast, 'element', node => {
     if (is(node, headings) && has(node, 'id')) {
-      const title = toString(node);
+      const title = toString({
+        children: excludeComponentFromChildren(node.children),
+        value: node.value,
+      });
 
       vFile.data.slugs.push({
         depth: parseInt(node.tagName[1], 10),
