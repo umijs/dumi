da67b226ece44e51e8adff38c897f3937cdd6a34
PeachScript <scdzwyxst@gmail.com>
2020-05-08 13:56:30 +0800

    test(routes): add test case for examples

---
+++ packages/preset-dumi/src/routes/fixtures/examples/examples/test.tsx
@@ -0,0 +1,3 @@
+/**
+ * title: testing example frontmatter
+ */
---
+++ packages/preset-dumi/src/routes/test/examples.test.ts
@@ -0,0 +1,74 @@
+import path from 'path';
+import { IApi } from '@umijs/types';
+import getRoutes from '../getRouteConfig';
+import { init } from '../../context';
+
+const FIXTURES_PATH = path.join(__dirname, '..', 'fixtures');
+
+describe('routes: examples', () => {
+  const cwd = path.join(FIXTURES_PATH, 'examples');
+
+  beforeAll(() => {
+    init({ cwd, paths: { cwd } } as IApi, {});
+  });
+
+  afterAll(() => {
+    init({} as IApi, {});
+  });
+
+  it('getExampleRoutes', () => {
+    const routes = getRoutes(
+      {
+        paths: {
+          cwd,
+          absPagesPath: path.join(cwd, 'src/pages'),
+        },
+      } as IApi,
+      {
+        locales: [],
+        resolve: {
+          examples: ['examples'],
+          includes: [],
+        },
+      },
+    );
+
+    expect(routes).toEqual([
+      {
+        path: '/_examples/test',
+        component: '../../examples/test.tsx',
+        title: 'testing example frontmatter',
+      },
+      {
+        path: '/',
+        component: '../../../../../themes/default/layout.js',
+        routes: [
+          {
+            path: '/',
+            component: '../../examples/index.md',
+            exact: true,
+            meta: {
+              filePath: 'examples/index.md',
+              updatedTime: 1588917390000,
+              slugs: [],
+              title: 'Index',
+            },
+            title: 'Index',
+          },
+          {
+            path: '/test',
+            component: '../../../../../themes/default/builtins/Example.js',
+            exact: true,
+            meta: {
+              title: 'testing example frontmatter',
+              example: true,
+              examplePath: '/_examples/test',
+            },
+            title: 'testing example frontmatter',
+          },
+        ],
+        title: undefined,
+      },
+    ]);
+  });
+});
