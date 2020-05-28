3bc52decb0b0670cfe8f7f5a47812f21b822bfb0
ShawDanon <shawdanon@foxmail.com>
2020-05-29 00:33:45 +0800

    feat: 处理windows路径问题

--- packages/create-dumi-lib/src/AppGenerator/AppGenerator.ts
+++ packages/create-dumi-lib/src/AppGenerator/AppGenerator.ts
@@ -1,13 +1,14 @@
-import { Generator } from '@umijs/utils';
+import { Generator, winPath } from '@umijs/utils';
 import { join } from 'path';
 
 export default class AppGenerator extends Generator {
   async writing() {
+    const cwd = winPath(this.cwd);
     this.copyDirectory({
       context: {
         version: require('../../package').version,
         siteMode: this.args.site,
-        packageName: this.cwd.split('/').pop(),
+        packageName: cwd.split('/').pop(),
       },
       path: join(__dirname, '../../templates/AppGenerator'),
       target: this.cwd,
