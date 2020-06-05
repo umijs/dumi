bfc3ac5d85d00ac2300d00f8c89559d1aea8d025
PeachScript <scdzwyxst@gmail.com>
2020-06-06 00:06:08 +0800

    test(transformer): add case for previewer

---
+++ packages/preset-dumi/src/transformer/fixtures/raw/remark-demo.md
@@ -0,0 +1,7 @@
+```jsx
+import React from 'react';
+
+export default () => <h1>Hello World!</h1>
+```
+
+<code src="./demo-missing-react.tsx" />
---
+++ packages/preset-dumi/src/transformer/test/remark-demo.test.ts
@@ -0,0 +1,20 @@
+import fs from 'fs';
+import path from 'path';
+import remark from '../remark';
+
+describe('demo example', () => {
+  const FILE_PATH = path.join(__dirname, '../fixtures/raw/remark-demo.md');
+
+  it('transform md to jsx', () => {
+    const result = remark(fs.readFileSync(FILE_PATH).toString(), {
+      fileAbsPath: FILE_PATH,
+      strategy: 'default',
+      previewLangs: [],
+    }).contents.toString();
+
+    // compare transform content
+    expect(result).toEqual(
+      fs.readFileSync(path.join(__dirname, '../fixtures/expect/remark-demo.html')).toString(),
+    );
+  });
+});
