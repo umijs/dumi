c8309009664208c84ac6b902dbb57f2242ccd81c
PeachScript <scdzwyxst@gmail.com>
2020-06-18 11:51:39 +0800

    feat(assets): add basic dumi assets types & example

---
+++ packages/assets-types/example.ts
@@ -0,0 +1,45 @@
+import AssetsPackage from './typings';
+
+const examplePackage: AssetsPackage = {
+  name: 'Ant Design',
+  package: 'antd',
+  assets: {
+    atoms: [
+      {
+        identifier: 'Button',
+        name: '按钮',
+        'name.en-US': 'Button',
+        props: {},
+      },
+    ],
+    examples: [
+      {
+        type: 'COMPONENT',
+        atomAssetId: 'Button',
+        designId: '1.按钮/1.危险按钮',
+        name: '危险按钮',
+        'name.en-US': 'Danger Button',
+        props: {
+          type: 'danger',
+          children: '危险按钮',
+        },
+      },
+      {
+        type: 'BLOCK',
+        atomAssetId: 'Button',
+        designId: '1.按钮/2.和输入框一起使用',
+        name: '和输入框一起使用',
+        'name.en-US': 'Use with Input component',
+        dependencies: {
+          'index.tsx': {
+            type: 'FILE',
+            value:
+              "import React from 'react';\nimport { Button, Input } from 'antd';\n\nexport default () => <><Input /><Button>Hello</Button></>",
+          },
+        },
+      },
+    ],
+  },
+};
+
+export default examplePackage;
---
+++ packages/assets-types/typings/example.d.ts
@@ -0,0 +1,64 @@
+export interface ExampleBaseAsset {
+  /**
+   * The export module identifier of related atom asset
+   */
+  atomAssetId: string;
+  /**
+   * The identifier from design draft of atom asset, such as Sketch symbol name
+   */
+  designId?: string;
+  /**
+   * Example asset name
+   */
+  name: string;
+  'name.en-US'?: string;
+  /**
+   * Example asset description
+   */
+  description?: string;
+  'description.en-US'?: string;
+}
+
+/**
+ * Block example asset type definition
+ * @note  for example, a danger Button component with form is a block exmaple asset
+ */
+export interface ExampleBlockAsset extends ExampleBaseAsset {
+  type: 'BLOCK';
+  /**
+   * The dependencies of example asset
+   * @note  a npm package can be used as a dependency, a local file alse can be used as a dependency
+   */
+  dependencies: {
+    [key: string]: {
+      /**
+       * Dependency type
+       * @note  NPM:  a npm package dependency, like antd
+       *        FILE: a local file dependency, like a .less file
+       */
+      type: 'NPM' | 'FILE';
+      /**
+       * Dependency value
+       * @note  NPM:  a npm package version or a specific git url
+       *        FILE: a local file content
+       */
+      value: string;
+    };
+  };
+}
+
+/**
+ * Component example bloc asset type definition
+ * @note  for example, a danger demo of Button component is a component exmaple asset
+ */
+export interface ExampleComponentAsset extends ExampleBaseAsset {
+  type: 'COMPONENT';
+  /**
+   * TODO: the props values of component
+   */
+  props: any;
+}
+
+type ExampleAsset = ExampleComponentAsset | ExampleBlockAsset;
+
+export default ExampleAsset;
