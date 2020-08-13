d4ffb2665a79ed7cfc73fbf58d9ed5e87b4d5302
PeachScript <scdzwyxst@gmail.com>
2020-08-14 01:03:18 +0800

    refactor(theme): use link for locale change button (#176)

--- packages/theme-default/src/components/LocaleSelect.tsx
+++ packages/theme-default/src/components/LocaleSelect.tsx
@@ -1,7 +1,7 @@
 import React, { FC, useContext } from 'react';
 // @ts-ignore
 import { history } from 'dumi';
-import { context } from 'dumi/theme';
+import { context, Link } from 'dumi/theme';
 import './LocaleSelect.less';
 
 const LocaleSelect: FC = () => {
@@ -11,22 +11,25 @@ const LocaleSelect: FC = () => {
   } = useContext(context);
   const firstDiffLocale = locales.find(({ name }) => name !== locale);
 
-  function handleLocaleChange(ev) {
+  function getLocaleTogglePath(target: string) {
     let newPathname = history.location.pathname.replace(`/${locale}`, '');
 
     // append locale prefix to path if it is not the default locale
-    if (ev.target.value !== locales[0].name) {
-      newPathname = `/${ev.target.value}${newPathname}`.replace(/\/$/, '');
+    if (target !== locales[0].name) {
+      newPathname = `/${target}${newPathname}`.replace(/\/$/, '');
     }
 
-    history.push(newPathname);
+    return newPathname;
   }
 
   return (
     Boolean(locales.length) && (
       <div className="__dumi-default-locale-select" data-locale-count={locales.length}>
         {locales.length > 2 ? (
-          <select value={locale} onChange={handleLocaleChange}>
+          <select
+            value={locale}
+            onChange={ev => history.push(getLocaleTogglePath(ev.target.value))}
+          >
             {locales.map(localeItem => (
               <option value={localeItem.name} key={localeItem.name}>
                 {localeItem.label}
@@ -34,9 +37,7 @@ const LocaleSelect: FC = () => {
             ))}
           </select>
         ) : (
-          <span onClick={() => handleLocaleChange({ target: { value: firstDiffLocale.name } })}>
-            {firstDiffLocale.label}
-          </span>
+          <Link to={getLocaleTogglePath(firstDiffLocale.name)}>{firstDiffLocale.label}</Link>
         )}
       </div>
     )
