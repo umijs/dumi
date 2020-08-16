b7db0c61c719f30d8c69034454cdd745ea51a5cf
mortalYoung <yangwei1@outlook.com>
2020-08-16 11:55:14 +0800

    fix(transformer): fix regex for previewer id

--- packages/preset-dumi/src/transformer/remark/previewer.ts
+++ packages/preset-dumi/src/transformer/remark/previewer.ts
@@ -21,11 +21,10 @@ function getPreviewerId(yaml: any, fileAbsPath: string) {
     // /path/to/md => path-to-md
     id = slash(fileAbsPath)
       // discard suffix like index.md
-      .replace(/(index)?\.\w+$/, '')
+      .replace(/(\/index)?\.\w+$/, '')
       .split('/')
       // get the last three levels
       .slice(-2)
-      .filter(Boolean)
       .join('-');
   }
 
