6cee8ea51736989e58c3f174a6330ce7d597bce5
PeachScript <scdzwyxst@gmail.com>
2020-08-31 17:36:58 +0800

    refactor(theme): update mobile device wrapper

--- packages/theme-mobile/src/components/Device.tsx
+++ packages/theme-mobile/src/components/Device.tsx
@@ -1,28 +1,41 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useContext } from 'react';
+import QRCode from 'qrcode.react';
+import { Link, context } from 'dumi/theme';
 import './Device.less';
 
-interface DeviceProps {
+interface IDeviceProps {
+  className?: string;
   url: string;
-  isSiteMode?:boolean;
 }
 
-const Device: FC<DeviceProps> = ({ url ,isSiteMode}) => {
+const Device: FC<IDeviceProps> = ({ url, className }) => {
+  const [renderKey, setRenderKey] = useState(Math.random());
+  const {
+    config: { mode },
+  } = useContext(context);
+
   return (
-    <div className="__dumi-default-device ios" data-site-mode={isSiteMode} >
-      <figure>
-        <svg className="__dumi-default-device__md-bar" viewBox="0 0 1384.3 40.3">
-          <path
-            className="st0"
-            d="M1343 5l18.8 32.3c.8 1.3 2.7 1.3 3.5 0L1384 5c.8-1.3-.2-3-1.7-3h-37.6c-1.5 0-2.5 1.7-1.7 3z"
-          />
-          <circle className="st0" cx="1299" cy="20.2" r="20" />
-          <path
-            className="st0"
-            d="M1213 1.2h30c2.2 0 4 1.8 4 4v30c0 2.2-1.8 4-4 4h-30c-2.2 0-4-1.8-4-4v-30c0-2.3 1.8-4 4-4zM16 4.2h64c8.8 0 16 7.2 16 16s-7.2 16-16 16H16c-8.8 0-16-7.2-16-16s7.2-16 16-16z"
-          />
-        </svg>
-        <iframe title="dumi mobile" src={url} />
-      </figure>
+    <div
+      className={['__dumi-default-device'].concat(className).join(' ')}
+      data-device-type="iOS"
+      data-mode={mode}
+    >
+      <div className="__dumi-default-device-status">
+        <span>dumi</span>
+        <span>10:24</span>
+      </div>
+      <iframe title="dumi-previewer" src={url} key={renderKey} />
+      <div className="__dumi-default-device-action">
+        <button
+          className="__dumi-default-icon"
+          role="refresh"
+          onClick={() => setRenderKey(Math.random())}
+        />
+        <button className="__dumi-default-icon" role="qrcode">
+          <QRCode value={url} size={96} />
+        </button>
+        <Link to={url} target="_blank" className="__dumi-default-icon" role="open-demo" />
+      </div>
     </div>
   );
 };
