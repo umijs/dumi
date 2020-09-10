c1af0d415b593b7c0324ce1b8eb4dd87cc7cbdf1
PeachScript <scdzwyxst@gmail.com>
2020-09-10 22:48:27 +0800

    test(theme): fix circular effects for useLocaleProps case

--- packages/preset-dumi/src/theme/hooks/useLocaleProps.test.ts
+++ packages/preset-dumi/src/theme/hooks/useLocaleProps.test.ts
@@ -3,7 +3,9 @@ import useLocaleProps from './useLocaleProps';
 
 describe('theme API: useLocaleProps', () => {
   it('should transform props by locale', () => {
-    const { result } = renderHook(() => useLocaleProps('en-US', { title: 2, 'title_en-US': 1 }));
+    const { result } = renderHook(props => useLocaleProps('en-US', props), {
+      initialProps: { title: 2, 'title_en-US': 1 },
+    });
 
     expect(result.current).toEqual({ title: 1 });
   });
