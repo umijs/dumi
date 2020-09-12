abe08f419839033995b54ff9fafd19cdd7f07285
mortalYoung <yangwei1@outlook.com>
2020-09-12 17:11:05 +0800

    fix(theme): distinguish En & Ch in layout

--- packages/theme-default/src/layout.tsx
+++ packages/theme-default/src/layout.tsx
@@ -53,8 +53,7 @@ const Layout: React.FC<IRouteComponentProps> = ({ children, location }) => {
     !meta.gapless;
   const isCN =
     locale === 'zh-CN' ||
-    (locale === '*' && locales[0]?.name === 'zh-CN') ||
-    /[\u4e00-\u9fa5]/.test(JSON.stringify(navs));
+    (locale === '*' && locales[0]?.name === 'zh-CN');
   let updatedTime: any = new Date(meta.updatedTime).toLocaleString();
   const repoPlatform = { github: 'GitHub', gitlab: 'GitLab' }[
     (repoUrl || '').match(/(github|gitlab)/)?.[1] || 'nothing'
