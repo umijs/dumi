a028f83053be9d47544e5895161e089b36656e4e
PeachScript <scdzwyxst@gmail.com>
2020-12-10 19:52:57 +0800

    docs: add basic usage of cn version

---
+++ docs/.demos/modal/content.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => (
+  <div>
+    <p>Some contents...</p>
+    <p>Some contents...</p>
+    <p>Some contents...</p>
+  </div>
+);
---
+++ docs/.demos/modal/modal.jsx
@@ -0,0 +1,54 @@
+/**
+ * title: 基础 Modal
+ * desc: 这是 antd Modal 组件的基础示例
+ */
+
+import React from 'react';
+import { Button, Modal } from 'antd';
+import ModalContent from './content';
+import './modal.less';
+
+class App extends React.Component {
+  state = { visible: false };
+
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  handleOk = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+
+  handleCancel = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Button type="primary" onClick={this.showModal}>
+          Open Modal
+        </Button>
+        <Modal
+          title="Basic Modal"
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+          className="example-modal-content"
+        >
+          <ModalContent />
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default () => <App />;
