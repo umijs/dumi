bdafa8d324cb160c14ef2c114ef133fd964ac20c
Bowen <Mr_lbw@outlook.com>
2020-12-29 18:03:11 +0800

    fix(theme): useDemoUrl should work with hash router fix (#445) (#449)
    * fix(preset): useDemoUrl should work with hash router fix#445
    
    * chore: code style

--- packages/preset-dumi/src/theme/hooks/useDemoUrl.test.ts
+++ packages/preset-dumi/src/theme/hooks/useDemoUrl.test.ts
@@ -2,10 +2,25 @@ import { renderHook } from '@testing-library/react-hooks';
 import useDemoUrl from './useDemoUrl';
 
 describe('theme API: useDemoUrl', () => {
+  const saved = window.location;
+  delete window.location;
+  window.location = {
+    ...saved,
+    href: `http://localhost/`,
+  } as Window['location'];
+
+  beforeEach(() => {
+    window.location.href = `http://localhost/`;
+  });
+
   it('should return normal demo url', () => {
     const { result } = renderHook(() => useDemoUrl('test'));
 
     expect(result.current).toEqual('http://localhost/~demos/test');
+
+    window.location.href += '#/';
+    const { result: hashResult } = renderHook(() => useDemoUrl('test'));
+    expect(hashResult.current).toEqual(`http://localhost/#/~demos/test`);
   });
 
   it('should return demo url and prefix router base', () => {
@@ -14,10 +29,22 @@ describe('theme API: useDemoUrl', () => {
 
     const { result } = renderHook(() => useDemoUrl('test'));
 
+    expect(result.current).toEqual('http://localhost/hello/~demos/test');
+
+    window.location.href += '#/';
+    const { result: hashResult } = renderHook(() => useDemoUrl('test'));
+    expect(hashResult.current).toEqual(`http://localhost/#/hello/~demos/test`);
+
+    window.location.href = `http://localhost/#/123#anchor`;
+    const { result: hashResult0 } = renderHook(() => useDemoUrl('test'));
+    expect(hashResult0.current).toEqual(`http://localhost/#/hello/~demos/test`);
+
+    window.location.href = `http://localhost/prefix/path/#/123#anchor`;
+    const { result: hashResult1 } = renderHook(() => useDemoUrl('test'));
+    expect(hashResult1.current).toEqual(`http://localhost/prefix/path/#/hello/~demos/test`);
+
     // @ts-ignore
     delete window.routerBase;
-
-    expect(result.current).toEqual('http://localhost/hello/~demos/test');
   });
 
   it('should return basement-compatible demo url', () => {
@@ -25,8 +52,21 @@ describe('theme API: useDemoUrl', () => {
 
     process.env.PLATFORM_TYPE = 'BASEMENT';
     const { result } = renderHook(() => useDemoUrl('test'));
-    process.env.PLATFORM_TYPE = oType;
 
     expect(result.current).toEqual('http://localhost/_demos/test/index.html');
+
+    window.location.href += '#/';
+    const { result: hashResult } = renderHook(() => useDemoUrl('test'));
+    expect(hashResult.current).toEqual(`http://localhost/#/_demos/test/index.html`);
+
+    window.location.href = `http://localhost/#/123#anchor`;
+    const { result: hashResult0 } = renderHook(() => useDemoUrl('test'));
+    expect(hashResult0.current).toEqual(`http://localhost/#/_demos/test/index.html`);
+
+    window.location.href = `http://localhost/prefix/path/#/123#anchor`;
+    const { result: hashResult1 } = renderHook(() => useDemoUrl('test'));
+    expect(hashResult1.current).toEqual(`http://localhost/prefix/path/#/_demos/test/index.html`);
+
+    process.env.PLATFORM_TYPE = oType;
   });
 });
