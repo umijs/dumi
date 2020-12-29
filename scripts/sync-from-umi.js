cadffde53d0e3b03457fc08dd1557d7a7d879d0d
PeachScript <scdzwyxst@gmail.com>
2020-12-29 19:22:38 +0800

    docs: make doc sync script can be running inside gh actions

--- scripts/sync-from-umi.js
+++ scripts/sync-from-umi.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 const path = require('path');
-const { execSync } = require('child_process');
+const https = require('https');
 
 const UMI_DOC_DIR = path.join(__dirname, '..', 'docs', '.upstream');
 const FILE_LIST = [
   // config docs
   {
     localname: 'config.md',
-    upstream: 'raw.githubusercontent.com/umijs/umi/master/docs/config/README.md',
+    upstream: 'https://cdn.jsdelivr.net/gh/umijs/umi@3/docs/config/README.md',
     actions: [
       // remove head content
       { type: 'slice', value: [13] },
@@ -30,30 +30,38 @@ const FILE_LIST = [
   },
 ];
 
+if (!fs.existsSync(UMI_DOC_DIR)) {
+  fs.mkdirSync(UMI_DOC_DIR);
+}
+
 // process files
 FILE_LIST.forEach(file => {
   const localPath = path.join(UMI_DOC_DIR, file.localname);
 
-  // pull files
-  execSync(`curl -o ${file.localname} -L ${file.upstream}`, { cwd: UMI_DOC_DIR, stdio: 'inherit' });
+  // get file from upstream
+  https.get(file.upstream, res => {
+    let content = '';
 
-  // execute process actions
-  let content = fs.readFileSync(localPath).toString();
+    res.on('data', chunk => { content += chunk });
+    res.on('end', () => {
+      // execute process actions
+      (file.actions || []).forEach(action => {
+        switch (action.type) {
+          case 'slice':
+            content = content.split(/\n/g).slice(action.value[0], action.value[1]).join('\n');
+            break;
 
-  (file.actions || []).forEach(action => {
-    switch (action.type) {
-      case 'slice':
-        content = content.split(/\n/g).slice(action.value[0], action.value[1]).join('\n');
-        break;
+          case 'replace':
+            content = content.replace(action.value[0], action.value[1]);
+            break;
 
-      case 'replace':
-        content = content.replace(action.value[0], action.value[1]);
-        break;
+          default:
+        }
+      });
 
-      default:
-    }
+      // write back to file
+      fs.writeFileSync(localPath, content);
+      console.log('sync', file.localname, 'from upstream successfully!');
+    });
   });
-
-  // write back to file
-  fs.writeFileSync(localPath, content);
 });
