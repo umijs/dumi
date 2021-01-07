cb81e5ceee09b0c32371fac54bf2321fc3600971
ahukpyu <ahukpyu@gmail.com>
2021-01-07 19:51:35 +0800

    fix: 解决覆盖率降低的问题

--- packages/preset-dumi/src/utils/getHostPkgAlias.ts
+++ packages/preset-dumi/src/utils/getHostPkgAlias.ts
@@ -16,25 +16,21 @@ function getPkgAliasForPath(absPath: string) {
 }
 
 export default (paths: IApi['paths']) => {
-  const catalogPattern = /EnumerateProviders (catalog|totalPro)=\d+/g;
   const isLerna = fs.existsSync(path.join(paths.cwd, 'lerna.json'));
   const pkgs: [string, string][] = [];
 
   if (isLerna) {
     // for lerna repo
     const { version: lernaVersion } = require('lerna/package.json');
-
     if (lernaVersion.startsWith('3')) {
-      let res = execSync(`${path.join(paths.cwd, 'node_modules/.bin/lerna')} ls --json --all`, {
+      JSON.parse(execSync(`${path.join(paths.cwd, 'node_modules/.bin/lerna')} ls --json --all`, {
         stdio: 'pipe',
-      }).toString();
-      // 修复windows环境下出现的 EnumerateProviders log导致的parse错误
-      if (catalogPattern.test(res)) {
-        res = res.replace(catalogPattern, "").trim()
-      }
-      JSON.parse(res).forEach(pkg => {
-        pkgs.push([pkg.name, pkg.location]);
-      });
+      })
+        .toString()
+        // fix: 修复windows环境下有多余输出导致JSON.parse报错的问题
+        .replace(/([\r\n]\])[^]*$/, '$1')).forEach(pkg => {
+          pkgs.push([pkg.name, pkg.location]);
+        });
     } else if (require.resolve('lerna/lib/PackageUtilities', { paths: [paths.cwd] })) {
       // reference: https://github.com/azz/lerna-get-packages/blob/master/index.js
       const PackageUtilities = require(require.resolve('lerna/lib/PackageUtilities', {
