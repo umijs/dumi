05a5979f56db3c1c965fec7f929bcb06e04d4b77
xrkffgg <xrkffgg@vip.qq.com>
2021-01-25 15:10:02 +0800

    chore: forbid package name "dumi"

--- packages/dumi/src/index.ts
+++ packages/dumi/src/index.ts
@@ -3,6 +3,11 @@ const { fork } = require('child_process');
 module.exports = () => {
   process.env.UMI_PRESETS = require.resolve('@umijs/preset-dumi');
 
+  if (process.env.npm_package_name === 'dumi') {
+    console.log("Do not use dumi as package.name !");
+    process.exit(1);
+  }
+
   // start umi use child process
   const child = fork(require.resolve('umi/bin/umi'), [...(process.argv.slice(2) || [])], {
     stdio: 'inherit',
