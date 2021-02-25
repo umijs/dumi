d12b31e3d581e8624a7f7a69b61f20dec4a74c15
PeachScript <scdzwyxst@gmail.com>
2021-02-26 00:00:53 +0800

    test(routes): add nav redirect case

---
+++ packages/preset-dumi/src/routes/test/redirect.test.ts
@@ -0,0 +1,47 @@
+import path from 'path';
+import getRoute from '../getRouteConfigFromDir';
+import decorateRoute from '../decorator';
+
+const FIXTURES_PATH = path.join(__dirname, '..', 'fixtures');
+
+const DEFAULT_LOCALES = [
+  ['en-US', 'EN'],
+  ['zh-CN', '中文'],
+];
+
+describe('routes: redirect', () => {
+  let routes = getRoute(path.join(FIXTURES_PATH, 'redirect'), {
+    locales: DEFAULT_LOCALES,
+  } as any);
+
+  it('nav redirect according to user menu config', () => {
+    routes = decorateRoute(
+      routes,
+      {
+        locales: DEFAULT_LOCALES,
+        mode: 'site',
+        menus: {
+          '/nav1': [
+            {
+              children: ['nav1/b'],
+            },
+          ],
+          '/nav2': [
+            {
+              children: [{ path: '/nav2/b' }],
+            },
+          ],
+        }
+      } as any,
+      {
+        paths: {
+          cwd: process.cwd(),
+          absTmpPath: path.join(process.cwd(), 'src/.umi'),
+        },
+      } as any,
+    );
+
+    expect(routes.find(route => route.path === '/nav1').redirect).toEqual('/nav1/b');
+    expect(routes.find(route => route.path === '/nav2').redirect).toEqual('/nav2/b');
+  });
+});
