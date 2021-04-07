105ceebb3a4e560941309e62267c87714ccd7015
xrkffgg <xrkffgg@vip.qq.com>
2021-04-07 16:21:42 +0800

    fix: remove katex

--- packages/preset-dumi/src/loader/index.ts
+++ packages/preset-dumi/src/loader/index.ts
@@ -2,8 +2,6 @@ import transformer from '../transformer';
 import getTheme from '../theme/loader';
 import { getFileRangeLines, getFileContentByRegExp } from '../utils/getFileContent';
 
-let useKatexFilePath = '';
-
 export default async function loader(raw: string) {
   let content = raw;
   const params = new URLSearchParams(this.resourceQuery);
@@ -20,21 +18,10 @@ export default async function loader(raw: string) {
   const result = transformer.markdown(content, this.resourcePath, { noCache: content !== raw });
   const theme = await getTheme();
 
-  // mark current file if it contains Katex and there has not another file used Katex
-  if (result.content.includes('className="katex"') && !useKatexFilePath) {
-    useKatexFilePath = this.resource;
-  }
-
   return `
     import React from 'react';
     import { dynamic } from 'dumi';
     import { Link, AnchorLink } from 'dumi/theme';
-    ${
-      // add Katex css import statement if required or not in production mode, to reduce dist size
-      useKatexFilePath === this.resource || process.env.NODE_ENV !== 'production'
-        ? "import 'katex/dist/katex.min.css';"
-        : ''
-    }
     ${theme.builtins
       .concat(theme.fallbacks)
       .map(component => `import ${component.identifier} from '${component.source}';`)
