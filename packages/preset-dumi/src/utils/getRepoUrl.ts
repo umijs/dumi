6af64fb84023d6a6cb05917c5a0ffab75ba0254a
xrkffgg <xrkffgg@vip.qq.com>
2021-05-06 22:46:10 +0800

    fix: add

--- packages/preset-dumi/src/utils/getRepoUrl.ts
+++ packages/preset-dumi/src/utils/getRepoUrl.ts
@@ -1,7 +1,7 @@
 import hostedGit from 'hosted-git-info';
 
 export default (url: string, platform?: 'gitlab') => {
-  if (!url) return '';
+  if (!url || typeof(url) !== 'string') return '';
   let repoUrl = hostedGit.fromUrl(url)?.browse();
 
   if (!repoUrl && url) {
