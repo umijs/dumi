01484676b2582a1ae188ac28353ec139feb9cba2
PeachScript <scdzwyxst@gmail.com>
2021-10-14 12:07:40 +0800

    perf(transformer): to avoid generate empty markdown wrapper node

--- packages/preset-dumi/src/transformer/remark/isolation.ts
+++ packages/preset-dumi/src/transformer/remark/isolation.ts
@@ -12,25 +12,47 @@ function isSoloNode(node: IDumiElmNode) {
   return Boolean(node.previewer || node.embed);
 }
 
+/**
+ * detect node whether line break node
+ * @param node  node
+ */
+function isLineBreakNode(node: IDumiElmNode) {
+  return node.type === 'text' && /^[\n\r]+$/.test(node.value as string);
+}
+
+/**
+ * detect node whether .markdown wrapper
+ * @param node      node
+ * @param className wrapper css class name
+ */
+function isWrapperNode(node: IDumiElmNode, className: string) {
+  return node.properties?.className === className;
+}
+
 const visitor: Visitor<IDumiElmNode> = function visitor(node) {
   // wrap all noddes except previewer nodes into markdown elements for isolate styles
   node.children = node.children.reduce((result, item) => {
-    if (isSoloNode(item)) {
-      // push item directly if it is solo node
+    let prevSibling = result[result.length - 1];
+    if (
+      isSoloNode(item) ||
+      (isLineBreakNode(item) && (!prevSibling || !isWrapperNode(prevSibling, this.className)))
+    ) {
+      // push item directly if it is solo node or is not break line node before content
       result.push(item);
     } else {
       // push wrapper element when first loop or the prev node is solo node
-      if (!result.length || isSoloNode(result[result.length - 1])) {
-        result.push({
+      if (!prevSibling || isSoloNode(prevSibling) || isLineBreakNode(prevSibling)) {
+        prevSibling = {
           type: 'element',
           tagName: 'div',
           properties: { className: this.className },
           children: [],
-        });
+        };
+        result.push(prevSibling);
       }
 
       // push item into wrapper element if it is not solo node
-      result[result.length - 1].children.push(item);
+      prevSibling.children.push(item);
     }
 
     return result;
