11569eb3ecff41da849af221a8a61c21c63b435a
xierenyuan <xierenyuan@qq.com>
2022-08-10 10:58:42 +0800

    chore: update prettier conf

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  pluginSearchDirs: false,
+  plugins: [
+    require.resolve('prettier-plugin-organize-imports'),
+    require.resolve('prettier-plugin-packagejson')
+  ],
+  printWidth: 80,
+  proseWrap: 'never',
+  singleQuote: true,
+  trailingComma: 'all'
+}
