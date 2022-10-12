caa1fa1bfe3424dc7b22af7fd599a73435707be7
PeachScript <scdzwyxst@gmail.com>
2022-10-12 18:03:29 +0800

    refactor: lazy load atom parser to keep typescript optional

--- src/features/parser.ts
+++ src/features/parser.ts
@@ -1,5 +1,6 @@
-import AtomAssetsParser from '@/assetParsers/atom';
+import type AtomAssetsParser from '@/assetParsers/atom';
 import type { IApi } from '@/types';
+import assert from 'assert';
 import { ATOMS_META_PATH } from './meta';
 
 export default (api: IApi) => {
@@ -27,19 +28,23 @@ export default (api: IApi) => {
 
   // auto-detect default entry file
   api.modifyDefaultConfig((memo) => {
-    if (!api.userConfig.resolve?.entryFile) {
-      // TODO: read entry from father config or support configured in config
-      api.logger.error(
-        '`resolve.entryFile` must be configured when `apiParser` enable',
-      );
-      process.exit(1);
-    }
+    // TODO: read entry from father config or support configured in config
+    assert(
+      api.userConfig.resolve?.entryFile,
+      '`resolve.entryFile` must be configured when `apiParser` enable',
+    );
+
+    assert(
+      api.pkg.devDependencies?.['typescript'],
+      'typescript must be installed when `apiParser` enable',
+    );
 
     return memo;
   });
 
   // share parser with other plugins via service
-  api.onStart(() => {
+  api.onStart(async () => {
+    const { default: AtomAssetsParser } = await import('@/assetParsers/atom');
     api.service.atomParser = new AtomAssetsParser({
       entryFile: api.config.resolve.entryFile!,
       resolveDir: api.cwd,
