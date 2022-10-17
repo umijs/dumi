599eeb594ad850da01a8ba75d6e0c457aebee938
wangyiyi <981290879@qq.com>
2022-10-17 12:23:59 +0800

    feat: rehypeImg unit testing

---
+++ src/loaders/markdown/transformer/fixtures/img-demo/expect.ts
@@ -0,0 +1,7 @@
+import type { IMdTransformerResult } from '../..';
+
+export default (ret: IMdTransformerResult) => {
+  expect(ret.content).toEqual(
+    "<><div className='markdown'><img src={require('../logo.png')} /></div></>",
+  );
+};
