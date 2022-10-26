fdbdb20eb564c0ec5fefc16d0c34ddc8e61fab63
wangyiyi <981290879@qq.com>
2022-10-27 00:41:09 +0800

    feat: add Table in theme default

---
+++ src/client/theme-default/builtins/Table/index.tsx
@@ -0,0 +1,50 @@
+import throttle from 'lodash.throttle';
+import React, {
+  useEffect,
+  useRef,
+  useState,
+  type FC,
+  type ReactNode,
+} from 'react';
+import './index.less';
+
+const Table: FC<{ children: ReactNode }> = ({ children }) => {
+  const container = useRef<HTMLDivElement>(null);
+  const [leftFolded, setLeftFolded] = useState(false);
+  const [rightFolded, setRightFolded] = useState(false);
+
+  // watch content scroll to render folded shadow
+  useEffect(() => {
+    const elm = container.current;
+    if (elm) {
+      const handler = throttle(() => {
+        setLeftFolded(elm.scrollLeft > 0);
+        setRightFolded(elm.scrollLeft < elm.scrollWidth - elm.offsetWidth);
+      }, 100);
+
+      handler();
+      elm.addEventListener('scroll', handler);
+      window.addEventListener('resize', handler);
+
+      return () => {
+        elm.removeEventListener('scroll', handler);
+        window.removeEventListener('resize', handler);
+      };
+    }
+  }, []);
+
+  return (
+    <div className="__dumi-default-table">
+      <div
+        className="__dumi-default-table-content"
+        ref={container}
+        data-left-folded={leftFolded || undefined}
+        data-right-folded={rightFolded || undefined}
+      >
+        <table>{children}</table>
+      </div>
+    </div>
+  );
+};
+
+export default Table;
