bf9885104c8ae2d09b2ff720ef11beb328a7f834
PeachScript <scdzwyxst@gmail.com>
2022-10-29 13:15:39 +0800

    fix: plugin register tab error on windows

--- src/features/tabs.ts
+++ src/features/tabs.ts
@@ -45,6 +45,7 @@ export default (api: IApi) => {
     // generate id for tabs
     tabsFromPlugins.forEach((tab) => {
       tab.id ??= `plugin-tab${tab.test ? `-${tab.test}` : ''}-${tab.key}`;
+      tab.component = winPath(tab.component);
     });
 
     return memo;
