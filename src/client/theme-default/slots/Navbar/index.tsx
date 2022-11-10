eb0b7ed17720b25bbdf50c422b1dec52fa1da259
miracles1919 <lujie@yuantutech.com>
2022-11-10 17:23:18 +0800

    fix: external nav link render error in default theme  (#1260)
    * fix(Navbar): handle outside links with `a` tag
    
    * fix: remove active

--- src/client/theme-default/slots/Navbar/index.tsx
+++ src/client/theme-default/slots/Navbar/index.tsx
@@ -10,15 +10,21 @@ const Navbar: FC = () => {
     <ul className="dumi-default-navbar">
       {nav.map((item) => (
         <li key={item.link}>
-          <Link
-            to={item.link}
-            {...(pathname.startsWith(item.activePath || item.link)
-              ? { className: 'active' }
-              : {})}
-          >
-            {item.title}
-            {/* TODO: 2-level nav */}
-          </Link>
+          {/^(\w+:)\/\/|^(mailto|tel):/.test(item.link) ? (
+            <a href={item.link} target="_blank" rel="noreferrer">
+              {item.title}
+            </a>
+          ) : (
+            <Link
+              to={item.link}
+              {...(pathname.startsWith(item.activePath || item.link)
+                ? { className: 'active' }
+                : {})}
+            >
+              {item.title}
+              {/* TODO: 2-level nav */}
+            </Link>
+          )}
         </li>
       ))}
     </ul>
