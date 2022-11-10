09e7fe894fce5007d7ca2f544cac17453ffb81ae
lujie <lujie@yuantutech.com>
2022-11-10 16:55:56 +0800

    fix: remove active

--- src/client/theme-default/slots/Navbar/index.tsx
+++ src/client/theme-default/slots/Navbar/index.tsx
@@ -8,27 +8,25 @@ const Navbar: FC = () => {
 
   return (
     <ul className="dumi-default-navbar">
-      {nav.map((item) => {
-        const cls = {
-          ...(pathname.startsWith(item.activePath || item.link)
-            ? { className: 'active' }
-            : {}),
-        };
-        return (
-          <li key={item.link}>
-            {/^(\w+:)\/\/|^(mailto|tel):/.test(item.link) ? (
-              <a href={item.link} target="_blank" rel="noreferrer" {...cls}>
-                {item.title}
-              </a>
-            ) : (
-              <Link to={item.link} {...cls}>
-                {item.title}
-                {/* TODO: 2-level nav */}
-              </Link>
-            )}
-          </li>
-        );
-      })}
+      {nav.map((item) => (
+        <li key={item.link}>
+          {/^(\w+:)\/\/|^(mailto|tel):/.test(item.link) ? (
+            <a href={item.link} target="_blank" rel="noreferrer">
+              {item.title}
+            </a>
+          ) : (
+            <Link
+              to={item.link}
+              {...(pathname.startsWith(item.activePath || item.link)
+                ? { className: 'active' }
+                : {})}
+            >
+              {item.title}
+              {/* TODO: 2-level nav */}
+            </Link>
+          )}
+        </li>
+      ))}
     </ul>
   );
 };
