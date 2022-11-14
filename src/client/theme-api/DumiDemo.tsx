2dbe3278c1b7e8c7089270963a2e5ab3f7a8a224
PeachScript <scdzwyxst@gmail.com>
2022-11-14 14:47:04 +0800

    feat: add ErrorBoundary for demo render

--- src/client/theme-api/DumiDemo.tsx
+++ src/client/theme-api/DumiDemo.tsx
@@ -1,7 +1,9 @@
 import { SP_ROUTE_PREFIX } from '@/constants';
 import { useSiteData } from 'dumi';
+import Container from 'dumi/theme/builtins/Container';
 import Previewer from 'dumi/theme/builtins/Previewer';
-import React, { createElement, type FC } from 'react';
+import React, { createElement, type FC, type ReactNode } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 import type { IPreviewerProps } from './types';
 
 export interface IDumiDemoProps {
@@ -12,11 +14,35 @@ export interface IDumiDemoProps {
   previewerProps: Omit<IPreviewerProps, 'asset' | 'children'>;
 }
 
+const DemoErrorBoundary: FC<{ children: ReactNode }> = (props) => (
+  <ErrorBoundary
+    fallbackRender={({ error }: any) => (
+      <Container type="error">
+        <p>
+          <strong>{error.message || 'This demo has been crashed.'}</strong>
+        </p>
+        {error.stack && (
+          <p>
+            <details open>
+              <summary>Error stack</summary>
+              <pre>{error.stack}</pre>
+            </details>
+          </p>
+        )}
+      </Container>
+    )}
+  >
+    {props.children}
+  </ErrorBoundary>
+);
+
 export const DumiDemo: FC<IDumiDemoProps> = (props) => {
   const { demos } = useSiteData();
   const { component, asset } = demos[props.demo.id];
 
-  if (props.demo.inline) return createElement(component);
+  if (props.demo.inline) {
+    return <DemoErrorBoundary>{createElement(component)}</DemoErrorBoundary>;
+  }
 
   return (
     <Previewer
@@ -28,7 +54,9 @@ export const DumiDemo: FC<IDumiDemoProps> = (props) => {
       }
       {...props.previewerProps}
     >
-      {props.previewerProps.iframe ? null : createElement(component)}
+      {props.previewerProps.iframe ? null : (
+        <DemoErrorBoundary>{createElement(component)}</DemoErrorBoundary>
+      )}
     </Previewer>
   );
 };
