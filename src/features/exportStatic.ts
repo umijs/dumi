c97bd5d8288fb035a48828b4a1046d225cf39a6d
PeachScript <scdzwyxst@gmail.com>
2022-11-16 11:35:54 +0800

    fix: extraRoutePaths from default config are lost

--- src/features/exportStatic.ts
+++ src/features/exportStatic.ts
@@ -3,21 +3,44 @@ import type { IApi } from '@/types';
 import { getExampleAssets } from './assets';
 
 export default (api: IApi) => {
+  const prevExtraRoutePaths: IApi['config']['exportStatic']['extraRoutePaths'][] =
+    [];
+
   api.describe({ key: 'dumi:exportStatic' });
+
+  // save extraRoutePaths from default config, will merge theme later
+  api.register({
+    key: 'modifyDefaultConfig',
+    stage: Infinity,
+    fn(memo: IApi['config']) {
+      if (memo.exportStatic && memo.exportStatic.extraRoutePaths) {
+        prevExtraRoutePaths.push(memo.exportStatic.extraRoutePaths);
+      }
+    },
+  });
 
   // static /~demos/:id pages when exportStatic enabled
   api.register({
     key: 'modifyConfig',
     stage: Infinity,
     fn(memo: IApi['config']) {
-      if (api.userConfig.exportStatic !== false) {
-        const prev = memo.exportStatic?.extraRoutePaths || [];
+      if (memo.exportStatic !== false) {
+        // save extraRoutePaths from config, will merge theme later
+        if (memo.exportStatic?.extraRoutePaths) {
+          prevExtraRoutePaths.push(memo.exportStatic.extraRoutePaths);
+        }
 
         memo.exportStatic ??= {};
         memo.exportStatic.extraRoutePaths = async () => {
           const examples = getExampleAssets();
-          const userExtraPaths =
-            typeof prev === 'function' ? await prev() : prev;
+          const userExtraPaths: string[] = [];
+
+          // merge all extraRoutePaths from default config & config
+          for (const prev of prevExtraRoutePaths) {
+            userExtraPaths.push(
+              ...(typeof prev === 'function' ? await prev() : prev),
+            );
+          }
 
           return userExtraPaths.concat(
             examples.map(({ id }) => `/${SP_ROUTE_PREFIX}demos/${id}`),
