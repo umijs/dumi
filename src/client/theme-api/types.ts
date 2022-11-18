014f3f5be0e3beab1ced103727e1aac48d5a9b96
xiaohuoni <xiaohuoni@dingtalk.com>
2022-11-18 22:27:53 +0800

    feat: h5 demo previewer

--- src/client/theme-api/types.ts
+++ src/client/theme-api/types.ts
@@ -156,6 +156,18 @@ export interface IThemeConfig {
   nav?: (INavItem & { children?: INavItem[] })[];
   sidebar?: Record<string, ISidebarGroup[]>;
   footer?: string;
+  /**
+   * hd solution configurations
+   * @note  base on https://github.com/umijs/umi-hd
+   */
+  hd: {
+    rules: {
+      minWidth?: number;
+      maxWidth?: number;
+      mode: 'vl' | 'flex' | 'vw' | 'vh';
+      options?: number | [number, number];
+    }[];
+  };
   [key: string]: any;
 }
 
