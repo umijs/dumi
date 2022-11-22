20d8e741b35abf35077f1d9d3eb66a90426fd604
ningzhongbin <ningzhongbin@meituan.com>
2022-11-22 15:32:02 +0800

    fix(code-block): 修复 codeBlock 编译 tsx 报错的问题

--- src/loaders/markdown/transformer/rehypeDemo.ts
+++ src/loaders/markdown/transformer/rehypeDemo.ts
@@ -243,7 +243,7 @@ export default function rehypeDemo(
               );
               component = techStack.transformCode(codeValue, {
                 type: 'code-block',
-                fileAbsPath: opts.fileAbsPath,
+                fileAbsPath: parseOpts.fileAbsPath,
               });
             }
 
