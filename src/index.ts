c817ceca322fdf0f586987f804ae6888533e1914
kiner-tang(文辉) <1127031143@qq.com>
2023-01-17 16:39:52 +0800

    feat: mfsu support pass true to use default mfsu config

--- src/index.ts
+++ src/index.ts
@@ -9,4 +9,10 @@ let unistUtilVisit: typeof import('unist-util-visit');
 
 export * from 'umi';
 export { unistUtilVisit };
-export const defineConfig = (config: IDumiUserConfig) => config;
+export const defineConfig = (config: IDumiUserConfig) => {
+  // delete the mfsu prop from config to use default mfsu config when pass ture
+  if (config.mfsu === true) {
+    delete config.mfsu;
+  }
+  return config;
+};
