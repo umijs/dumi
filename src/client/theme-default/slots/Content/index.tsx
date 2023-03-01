44eb2cff8a682babfeff728c18f2a92b68fe6caa
wuxh <wxh1220@gmail.com>
2023-03-02 03:03:35 +0800

    chore(theme): improve the default theme style

--- src/client/theme-default/slots/Content/index.tsx
+++ src/client/theme-default/slots/Content/index.tsx
@@ -1,15 +1,17 @@
-import { useSidebarData } from 'dumi';
+import { useSidebarData, useSiteData } from 'dumi';
 import React, { type FC, type ReactNode } from 'react';
 import './heti.scss';
 import './index.less';
 
 const Content: FC<{ children: ReactNode }> = (props) => {
   const sidebar = useSidebarData();
+  const { themeConfig } = useSiteData();
 
   return (
     <div
       className="dumi-default-content"
       data-no-sidebar={!sidebar || undefined}
+      data-has-footer={!!themeConfig.footer || undefined}
     >
       {props.children}
     </div>
