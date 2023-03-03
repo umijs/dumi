bebc26dbe5464c69cd3cfb7cb09d8a3b1af0f45a
Wuxh <wxh1220@gmail.com>
2023-03-03 10:07:24 +0800

    refactor: improve content padding when disable footer (#1517)
    * chore(theme): improve the default theme style
    
    * chore: update Content style
    
    * chore: update

--- src/client/theme-default/slots/Content/index.tsx
+++ src/client/theme-default/slots/Content/index.tsx
@@ -1,15 +1,17 @@
-import { useSidebarData } from 'dumi';
+import { useSidebarData, useSiteData } from 'dumi';
 import React, { type FC, type ReactNode } from 'react';
 import './heti.scss';
 import './index.less';
 
 const Content: FC<{ children: ReactNode }> = (props) => {
   const sidebar = useSidebarData();
+  const { themeConfig } = useSiteData();
 
   return (
     <div
       className="dumi-default-content"
       data-no-sidebar={!sidebar || undefined}
+      data-no-footer={themeConfig.footer === false || undefined}
     >
       {props.children}
     </div>
