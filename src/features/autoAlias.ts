b7b9050fd8273642edc75c2cb50f43a65ef85a8c
PeachScript <scdzwyxst@gmail.com>
2023-03-10 18:01:32 +0800

    refactor: correct autoAlias logic

--- src/features/autoAlias.ts
+++ src/features/autoAlias.ts
@@ -4,49 +4,55 @@ import fs from 'fs';
 import path from 'path';
 
 export default (api: IApi) => {
+  let entryDir: string;
+
   api.describe({
     key: 'autoAlias',
     config: {
       schema: (Joi) => Joi.bool(),
     },
-    enableBy: ({ userConfig }) => userConfig.autoAlias !== false,
+    enableBy: () => !!api.pkg.name,
   });
 
   api.modifyAppData(async (memo) => {
+    if (api.config.resolve?.entryFile) {
+      entryDir = path.resolve(api.cwd, api.config.resolve.entryFile);
+    } else if (fs.existsSync(path.join(api.cwd, 'src'))) {
+      entryDir = path.join(api.cwd, 'src');
+    }
+
     // save father configs to appData, allow other plugins to modify
     memo.fatherConfigs = await tryFatherBuildConfigs(api.cwd);
 
     return memo;
   });
 
-  api.modifyConfig(async (memo) => {
-    let entryDir = '';
+  api.chainWebpack((memo) => {
+    const fatherConfigs = api.appData.fatherConfigs as any[];
 
-    if (memo.resolve?.entryFile) {
-      entryDir = path.resolve(api.cwd, memo.resolve.entryFile);
-    } else if (fs.existsSync(path.join(api.cwd, 'src'))) {
-      entryDir = path.join(api.cwd, 'src');
-    }
+    // sort by output level, make sure the deepest output has the highest priority
+    fatherConfigs.sort((a, b) => {
+      const aLevel = (a.output?.path || a.output).split('/').length;
+      const bLevel = (b.output?.path || b.output).split('/').length;
 
-    if (entryDir && api.pkg.name) {
-      const fatherConfigs = api.appData.fatherConfigs as any[];
+      return bLevel - aLevel;
+    });
 
-      // sort by output level, make sure the deepest output has the highest priority
-      fatherConfigs.sort((a, b) => {
-        const aLevel = (a.output?.path || a.output).split('/').length;
-        const bLevel = (b.output?.path || b.output).split('/').length;
+    // create subpaths alias for each input/entry
+    fatherConfigs.forEach((item) => {
+      const key = `${api.pkg.name}/${item.output?.path || item.output}`;
 
-        return bLevel - aLevel;
-      });
+      if (!memo.resolve.alias.has(key)) {
+        memo.resolve.alias.set(
+          key,
+          path.join(api.cwd, item.entry || item.input),
+        );
+      }
+    });
 
-      // create subpaths alias for each input/entry
-      fatherConfigs.forEach((item) => {
-        memo.alias[`${api.pkg.name}/${item.output?.path || item.output}`] ??=
-          path.join(api.cwd, item.entry || item.input);
-      });
-
-      // create pkg alias to entry dir
-      memo.alias[api.pkg.name] ??= entryDir;
+    // create pkg alias to entry dir
+    if (entryDir && !memo.resolve.alias.has(api.pkg.name!)) {
+      memo.resolve.alias.set(api.pkg.name!, entryDir);
     }
 
     return memo;
