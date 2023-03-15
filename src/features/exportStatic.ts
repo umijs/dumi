a594b65842be7112a76562bbb9c2541cb5833b60
PeachScript <scdzwyxst@gmail.com>
2023-03-15 21:39:46 +0800

    refactor: improve the way to export static demo routes

--- src/features/exportStatic.ts
+++ src/features/exportStatic.ts
@@ -1,71 +1,25 @@
 import { SP_ROUTE_PREFIX } from '@/constants';
 import type { IApi } from '@/types';
-import { semver } from 'umi/plugin-utils';
 import { getExampleAssets } from './assets';
 
 export default (api: IApi) => {
-  const prevExtraRoutePaths: NonNullable<
-    IApi['config']['exportStatic']
-  >['extraRoutePaths'][] = [];
-
   api.describe({ key: 'dumi:exportStatic' });
 
-  // save extraRoutePaths from default config, will merge theme later
-  api.register({
-    key: 'modifyDefaultConfig',
-    stage: Infinity,
-    fn(memo: IApi['config']) {
-      if (memo.exportStatic && memo.exportStatic.extraRoutePaths) {
-        prevExtraRoutePaths.push(memo.exportStatic.extraRoutePaths);
-      }
-    },
-  });
-
   // static /~demos/:id pages when exportStatic enabled
-  api.register({
-    key: 'modifyConfig',
-    stage: Infinity,
-    fn(memo: IApi['config']) {
-      // @ts-ignore
-      if (memo.exportStatic !== false) {
-        // save extraRoutePaths from config, will merge theme later
-        if (memo.exportStatic?.extraRoutePaths) {
-          prevExtraRoutePaths.push(memo.exportStatic.extraRoutePaths);
-        }
+  api.modifyExportHTMLFiles((htmlFiles) => {
+    const filePrefix = `${SP_ROUTE_PREFIX}demos`;
+    const examples = getExampleAssets();
+    const content = htmlFiles.find((file) =>
+      file.path.startsWith(`${filePrefix}/:id/`),
+    )!.content;
 
-        memo.exportStatic ??= {};
-        memo.exportStatic.extraRoutePaths = async () => {
-          const examples = getExampleAssets();
-          const userExtraPaths: (
-            | string
-            | { path: string; prerender: boolean }
-          )[] = [];
-          // Umi support disable prerender since 4.0.48
-          const isSupportDisablePrerender = semver.gte(
-            api.appData.umi.version,
-            '4.0.48',
-          );
+    htmlFiles.push(
+      ...examples.map(({ id }) => ({
+        path: `${filePrefix}/${id}/index.html`,
+        content,
+      })),
+    );
 
-          // merge all extraRoutePaths from default config & config
-          for (const prev of prevExtraRoutePaths) {
-            userExtraPaths.push(
-              ...(typeof prev === 'function' ? await prev() : prev),
-            );
-          }
-
-          return userExtraPaths.concat(
-            examples.map(({ id }) => {
-              const demoPath = `/${SP_ROUTE_PREFIX}demos/${id}`;
-
-              return isSupportDisablePrerender
-                ? { path: demoPath, prerender: false }
-                : demoPath;
-            }),
-          );
-        };
-      }
-
-      return memo;
-    },
+    return htmlFiles;
   });
 };
