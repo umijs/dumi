ace93119a07389e72c37f2b3d94aa5b3ae449110
leegsen7 <>
2023-05-30 16:56:30 +0800

    feat: theme-mobile show qrcode previewer only in mobile model

--- suites/theme-mobile/src/slots/PreviewerActions/index.tsx
+++ suites/theme-mobile/src/slots/PreviewerActions/index.tsx
@@ -1,14 +1,18 @@
 import { ReactComponent as IconQrcode } from '@ant-design/icons-svg/inline-svg/outlined/qrcode.svg';
+import { useRouteMeta } from 'dumi';
 import PreviewerActions from 'dumi/theme-default/slots/PreviewerActions';
 import QRCode from 'qrcode.react';
 import React from 'react';
 import './index.less';
 
 const MobilePreviewerActions: typeof PreviewerActions = (props) => {
+  const {
+    frontmatter: { mobile = true },
+  } = useRouteMeta();
   const qrcodeUrl = `${location.origin}${props.demoUrl}`;
   const extra = (
     <>
-      {props.demoUrl && (
+      {mobile && props.demoUrl && (
         <button
           className="dumi-default-previewer-action-btn dumi-mobile-previewer-action-qrcode"
           type="button"
