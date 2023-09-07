a3a5032473cf947ed323ce21decc278d506b1302
二货机器人 <smith3816@gmail.com>
2023-09-07 15:28:01 +0800

    docs: add customize page

--- examples/normal/.dumi/pages/loader-test.tsx
+++ examples/normal/.dumi/pages/loader-test.tsx
@@ -1,7 +1,2 @@
-// console.log(getAllRoutesMeta());
-// console.log(getRouteMetaById(xx)); // xx 可以从 `.dumi/tmp/core/route.ts` 里找一个 Markdown 路由的 id
-// console.log(getDemoById('foo-demo-0'));
-
-console.log('>>>');
-
-export default () => '自定义 React 页面测试';
+// Customize Page for dumi test
+export default () => 'Customize Dumi Test Page';
