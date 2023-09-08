3528057c3216db8f2ff8a453fde706dfa6b08c33
MadCcc <1075746765@qq.com>
2023-09-08 12:31:28 +0800

    feat: split markdown meta loader (#1875)
    * feat: split mdLoader type
    
    * feat: split mdLoader
    
    * chore: code optimize
    
    * fix: use winPath
    
    * chore: update getter

--- examples/normal/.dumi/pages/loader-test.tsx
+++ examples/normal/.dumi/pages/loader-test.tsx
@@ -1,2 +1,12 @@
+import * as demo from '../../docs/index.md?type=demo';
+import * as demoIndex from '../../docs/index.md?type=demo-index';
+import * as frontmatter from '../../docs/index.md?type=frontmatter';
+import * as text from '../../docs/index.md?type=text';
+
+console.log('frontmatter', frontmatter);
+console.log('demo', demo);
+console.log('demoIndex', demoIndex);
+console.log('text', text);
+
 // Customize Page for dumi test
 export default () => 'Customize Dumi Test Page';
