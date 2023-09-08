6bc6d2193b28cb2b1ad5c2910d8eaf523c94523c
二货机器人 <smith3816@gmail.com>
2023-09-08 13:07:37 +0800

    chore: split code

---
+++ src/client/theme-api/DumiDemo/index.tsx
@@ -0,0 +1,23 @@
+import React, { type FC } from 'react';
+import type { IPreviewerProps } from '../types';
+import DumiDemoBlock from './DemoBlock';
+
+export interface IDumiDemoProps {
+  demo: {
+    id: string;
+    inline?: boolean;
+  };
+  previewerProps: Omit<IPreviewerProps, 'asset' | 'children'>;
+}
+
+export const DumiDemo: FC<IDumiDemoProps> = React.memo(
+  (props) => <DumiDemoBlock {...props} />,
+  (prev, next) => {
+    // compare length for performance
+    return JSON.stringify(prev).length === JSON.stringify(next).length;
+  },
+);
+
+if (process.env.NODE_ENV !== 'production') {
+  DumiDemo.displayName = 'DumiDemo';
+}
---
+++ src/client/theme-api/DumiDemo/use.ts
@@ -0,0 +1,26 @@
+// Copy from React official demo.
+// This will be replace if React release new version of use hooks
+function use(promise: Promise<any>) {
+  if (promise.status === 'fulfilled') {
+    return promise.value;
+  } else if (promise.status === 'rejected') {
+    throw promise.reason;
+  } else if (promise.status === 'pending') {
+    throw promise;
+  } else {
+    promise.status = 'pending';
+    promise.then(
+      (result) => {
+        promise.status = 'fulfilled';
+        promise.value = result;
+      },
+      (reason) => {
+        promise.status = 'rejected';
+        promise.reason = reason;
+      },
+    );
+    throw promise;
+  }
+}
+
+export default use;
