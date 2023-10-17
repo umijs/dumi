d6ab8202da8a4d236e291b4d274848cf59c0078e
MadCcc <1075746765@qq.com>
2023-10-17 11:40:30 +0800

    feat: make LiveEditor derivate Editor

--- src/client/theme-default/slots/LiveEditor/index.tsx
+++ src/client/theme-default/slots/LiveEditor/index.tsx
@@ -1,17 +1,20 @@
 import { LiveContext } from 'dumi';
 import { highlight, languages } from 'prismjs';
-import React, { FC, useContext } from 'react';
+import React, { ComponentProps, FC, useContext } from 'react';
 import Editor from 'react-simple-code-editor';
 
-const LiveEditor: FC = () => {
+const LiveEditor: FC<ComponentProps<typeof Editor>> = (props) => {
   const { code, onCodeChange } = useContext(LiveContext);
 
   return (
     <Editor
+      {...props}
       value={code}
       onValueChange={onCodeChange}
-      highlight={(code) => highlight(code, languages.js, 'tsx')}
-      padding={20}
+      highlight={
+        props.highlight ?? ((code) => highlight(code, languages.js, 'tsx'))
+      }
+      padding={props.padding ?? 20}
     />
   );
 };
