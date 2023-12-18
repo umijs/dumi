0fa9f9ca96a0f4c3eaa0cd1e32a4af78fbac040c
kiner-tang <1127031143@qq.com>
2023-12-18 15:36:03 +0800

    fix: preferer color may unexpected when using auto mode (#1989)

--- src/client/theme-api/usePrefersColor.ts
+++ src/client/theme-api/usePrefersColor.ts
@@ -86,7 +86,7 @@ class ColorChanger {
    * listen color change
    * @param cb  callback
    */
-  listen(cb: typeof this.callbacks[number]) {
+  listen(cb: (typeof this.callbacks)[number]) {
     this.callbacks.push(cb);
   }
 
@@ -94,7 +94,7 @@ class ColorChanger {
    * unlisten color change
    * @param cb  callback
    */
-  unlisten(cb: typeof this.callbacks[number]) {
+  unlisten(cb: (typeof this.callbacks)[number]) {
     this.callbacks.splice(this.callbacks.indexOf(cb), 1);
   }
 
@@ -108,7 +108,7 @@ class ColorChanger {
     this.prefersColor = color;
     this.color =
       color === 'auto' ? (this.isColorMode('dark') ? 'dark' : 'light') : color;
-    document.documentElement.setAttribute(PREFERS_COLOR_ATTR, color);
+    document.documentElement.setAttribute(PREFERS_COLOR_ATTR, this.color);
     this.applyCallbacks();
 
     return color;
