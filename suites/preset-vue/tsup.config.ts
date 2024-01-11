1e6c37df7acfb4efdf29777f1db81254d8f45d30
jeffwcx <jeffwcx@icloud.com>
2024-01-11 23:35:19 +0800

    fix: issue introduced by @vue/babel-plugin-jsx
    https://github.com/vuejs/babel-plugin-jsx/issues/687

--- suites/preset-vue/tsup.config.ts
+++ suites/preset-vue/tsup.config.ts
@@ -1,3 +1,4 @@
+import fs from 'node:fs';
 import { defineConfig } from 'tsup';
 
 export default defineConfig([
@@ -14,6 +15,22 @@ export default defineConfig([
     external: ['vue/compiler-sfc'],
     dts: true,
     treeshake: true,
-    minify: true,
+    esbuildPlugins: [
+      {
+        name: 'resolve-babel-plugin-jsx',
+        setup(build) {
+          build.onLoad({ filter: /babel-plugin-jsx.*mjs$/ }, async (args) => {
+            const code = await fs.promises.readFile(args.path, 'utf-8');
+            return {
+              loader: 'js',
+              contents: code.replace(
+                'import syntaxJsx from "@babel/plugin-syntax-jsx";',
+                'const syntaxJsx = require("@babel/plugin-syntax-jsx").default;',
+              ),
+            };
+          });
+        },
+      },
+    ],
   },
 ]);
