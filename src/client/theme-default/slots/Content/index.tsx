27b0793fb06ea746abfec4368e396467c559693a
Jinbao1001 <nodewebli@gmail.com>
2024-05-06 13:35:42 +0800

    chore: format css

--- src/client/theme-default/slots/Content/index.tsx
+++ src/client/theme-default/slots/Content/index.tsx
@@ -1,6 +1,6 @@
 import { useRouteMeta, useSidebarData, useSiteData } from 'dumi';
 import React, { type FC, type ReactNode } from 'react';
-import '../../../../../compiled/heti/heti.css';
+import './heti.less';
 import './index.less';
 
 const Content: FC<{ children: ReactNode }> = (props) => {
