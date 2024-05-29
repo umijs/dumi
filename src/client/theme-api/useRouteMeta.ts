d4d5301bb5f5ef18acf592b1eba27e7dde92ec58
Jinbao1001 <nodewebli@gmail.com>
2024-05-29 14:29:26 +0800

    fix: merge route.metas to metas

--- src/client/theme-api/useRouteMeta.ts
+++ src/client/theme-api/useRouteMeta.ts
@@ -1,3 +1,4 @@
+import deepmerge from 'deepmerge';
 import {
   getRouteMetaById,
   matchRoutes,
@@ -27,8 +28,11 @@ function getCachedRouteMeta(route: IRoutesById[string]) {
         Object.keys(route.meta).forEach((key) => {
           (meta as any)[key] ??= (route.meta as any)[key];
         });
+        meta['frontmatter'] = deepmerge(
+          meta['frontmatter'],
+          route.meta['frontmatter'],
+        );
       }
-
       return meta;
     };
     const meta = merge(getRouteMetaById(route.id, { syncOnly: true }));
