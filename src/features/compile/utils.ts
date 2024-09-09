d87c2072ca846b3fd47325c00db4d42c4a628bb8
Jinbao1001 <nodewebli@gmail.com>
2024-09-09 11:06:47 +0800

    docs: guide to faq

--- src/features/compile/utils.ts
+++ src/features/compile/utils.ts
@@ -1,16 +1,16 @@
+import { logger } from '@umijs/utils';
 import { isArray } from '@umijs/utils/compiled/lodash';
 import { IApi } from 'umi';
-
 export const shouldDisabledLiveDemo = (api: IApi) => {
   const extraBabelPlugins = api.userConfig.extraBabelPlugins;
   const disableFlag =
     isArray(extraBabelPlugins) &&
-    extraBabelPlugins?.some((p: any) =>
+    extraBabelPlugins!.some((p: any) =>
       /^import$|babel-plugin-import/.test(p[0]),
     );
   if (disableFlag) {
-    api.logger.warn(
-      "dumi don't suggest to use babel-plugin-import, liveDemo will be disabled because of this config.",
+    logger.warn(
+      'live demo feature has been automatically disabled since babel-plugin-import be registered, if you want to enable live demo feature, checkout: https://d.umijs.org/guide/faq',
     );
   }
   return disableFlag;
