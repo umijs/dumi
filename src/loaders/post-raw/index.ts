ed511070850c038fa10e58be1cb7c553d367c486
Jinbao1001 <nodewebli@gmail.com>
2024-09-13 10:50:13 +0800

    fix: winpath

--- src/loaders/post-raw/index.ts
+++ src/loaders/post-raw/index.ts
@@ -1,9 +1,10 @@
+import { winPath } from '@umijs/utils';
 /**
  * loader for mako dumi-raw watch-parent
  */
 export default function postRawLoader(this: any, raw: string) {
   return `
-  import '${this.resourcePath}?watch=parent';
+  import '${winPath(this.resourcePath)}?watch=parent';
   ${raw};
   `;
 }
