1266b62d961caf53f6dbf6fa85a75b94de527a35
Jinbao1001 <nodewebli@gmail.com>
2024-11-21 16:31:54 +0800

    fix: ci

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
+    testTimeout: 15000,
     alias: {
       '@': path.join(__dirname, 'src'),
     },
