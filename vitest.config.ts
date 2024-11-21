647865e41e9e7ce855d00a8aff9fdb75e7e86623
Jinbao1001 <nodewebli@gmail.com>
2024-11-21 17:34:01 +0800

    fix: ci

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,11 +3,19 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    sequence: {
+      hooks: 'parallel',
+    },
+    poolOptions: {
+      forks: {
+        singleFork: true,
+      },
+    },
     globals: true,
     testTimeout: 15000,
     alias: {
       '@': path.join(__dirname, 'src'),
     },
-    poolMatchGlobs: [['**/__tests__/**/*.fork.test.*', 'threads']],
+    poolMatchGlobs: [['**/__tests__/**/*.fork.test.*', 'forks']],
   },
 });
