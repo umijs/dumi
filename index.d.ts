b4eebf146cecadc184df395c4268dbb0e9c46346
zoomdong <1344492820@qq.com>
2026-06-08 12:21:54 +0800

    fix: declare dumi runtime exports

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,5 @@
+import type { IRouteMeta, IRoutesById } from './theme-api';
+
 export { Root as HastRoot } from 'hast';
 export * from 'umi';
 export {
@@ -14,6 +16,50 @@ export type {
   IDumiUserConfig,
 } from './dumi-types';
 export * from './theme-api';
+
+export declare enum ApplyPluginsType {
+  add = 'add',
+  modify = 'modify',
+  event = 'event',
+  compose = 'compose',
+}
+export declare class PluginManager {
+  static create(opts: Record<string, any>): PluginManager;
+  applyPlugins(opts: Record<string, any>): any;
+  [key: string]: any;
+}
+
+export declare const getSketchJSON: (
+  element: Element,
+  opts: Record<string, any>,
+) => Promise<Record<string, any>>;
+export declare const history: {
+  location: Location;
+  push(to: string | Record<string, any>): void;
+  replace(to: string | Record<string, any>): void;
+  listen(listener: (...args: any[]) => void): () => void;
+  [key: string]: any;
+};
+export declare const Helmet: any;
+export declare const Link: any;
+export declare const NavLink: any;
+export declare const matchRoutes: (...args: any[]) => any;
+export declare const useAppData: () => {
+  clientRoutes: any[];
+  routes: IRoutesById;
+  [key: string]: any;
+};
+export declare const useLocation: () => Location & Record<string, any>;
+export declare const useOutlet: () => any;
+export declare const useParams: () => Record<string, string | undefined>;
+export declare const useRouteData: () => {
+  route: { path?: string; meta?: IRouteMeta; [key: string]: any };
+  [key: string]: any;
+};
+export declare const useSearchParams: () => [
+  URLSearchParams,
+  (...args: any[]) => void,
+];
 
 export declare let unistUtilVisit: typeof import('unist-util-visit');
 export declare const getProjectRoot: (cwd: string) => string;
