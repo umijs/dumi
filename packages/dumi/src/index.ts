cc8a8194882a3385eb593760e39edc9f17b4140c
zoomdong <1344492820@qq.com>
2026-07-23 12:33:11 +0800

    chore: update dumi 1 bundler ad

--- packages/dumi/src/index.ts
+++ packages/dumi/src/index.ts
@@ -3,8 +3,19 @@ const { fork } = require('child_process');
 module.exports = () => {
   process.env.UMI_PRESETS = require.resolve('@umijs/preset-dumi');
 
+  const cliArgs = process.argv.slice(2) || [];
+  const shouldPrintUtoopackAd = cliArgs[0] === 'dev' && process.env.MAKO_AD !== 'none';
+
+  // Umi 3 promotes Mako during dev, but dumi 2 supports utoopack instead.
+  process.env.MAKO_AD = 'none';
+  if (shouldPrintUtoopackAd) {
+    process.stdout.write(
+      'Utoopack https://github.com/utooland/utoo is a fast Rust-based bundler for dumi 2. Upgrade to dumi 2 and enable it with `utoopack: {}`. Visit https://d.umijs.org/config#utoopack for more details.\n',
+    );
+  }
+
   // start umi use child process
-  const child = fork(require.resolve('umi/bin/umi'), [...(process.argv.slice(2) || [])], {
+  const child = fork(require.resolve('umi/bin/umi'), cliArgs, {
     stdio: 'inherit',
   });
 
